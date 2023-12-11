274b35292b19cb84d3b4fe4aebcaf12f0c481ef6
SyerliNP <serlynindi680@gmail.com>
2023-12-11 15:40:44 +0700

    css template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,20 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+// Route::get('/dashboard_admin', function (){
+//     return view('admin.dashboard_admin');
+// })->name('dashboard_admin');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/dashboard_admin', [AdminController::class, 'admin'])->name('dashboard_admin');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
